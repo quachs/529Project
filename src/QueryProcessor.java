cb683930ed58bd990fea0f1b1865cc910dca0997
Sylvia Quach <esquach@gmail.com>
2017-09-27 23:40:02 -0700

    Update author query

--- src/QueryProcessor.java
+++ src/QueryProcessor.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -11,73 +12,69 @@ Class to process a query.
  */
 public class QueryProcessor {
 
-    private static List<List<PositionalPosting>> AndCollection = new ArrayList<List<PositionalPosting>>(); 
-    
+    private static List<List<PositionalPosting>> AndCollection = new ArrayList<List<PositionalPosting>>();
+
     //Add the positional postings list of an AND query to the 
     //collection of AND query positional postings lists
-    private static void addAndQuery(Subquery andQueryLiterals, PositionalInvertedIndex posIndex, 
-            KGramIndex kgIndex){
-        
+    private static void addAndQuery(Subquery andQueryLiterals, PositionalInvertedIndex posIndex,
+            KGramIndex kgIndex) {
+
         List<PositionalPosting> masterList = new ArrayList<PositionalPosting>();
         String preLiteral = andQueryLiterals.getLiterals().get(0);
         List<PositionalPosting> intermediateList = new ArrayList<PositionalPosting>();
-        
-        if(preLiteral.contains("\"")){       
+
+        if (preLiteral.contains("\"")) {
             masterList = Phrase.phraseQuery(preLiteral, posIndex);
-        }
-        else if (preLiteral.contains("*")){
-            masterList = QueryProcessor.wildcardQuery(preLiteral, posIndex, kgIndex);  
-        }
-        else{
+        } else if (preLiteral.contains("*")) {
+            masterList = QueryProcessor.wildcardQuery(preLiteral, posIndex, kgIndex);
+        } else {
             masterList = posIndex.getPostingsList(preLiteral);
         }
-                        
+
         //Merge all of the postings lists of each. 
         //query literal of a given AND query into one master postings list. 
-        if (andQueryLiterals.getSize() > 1){
-            
-            for(int i = 1; i < andQueryLiterals.getSize(); i++){
+        if (andQueryLiterals.getSize() > 1) {
+
+            for (int i = 1; i < andQueryLiterals.getSize(); i++) {
                 String currentLiteral = andQueryLiterals.getLiterals().get(i);
 
-                if(currentLiteral.contains("\"")){        
+                if (currentLiteral.contains("\"")) {
                     intermediateList = Phrase.phraseQuery(currentLiteral, posIndex);
                     masterList = ListMerge.intersectList(masterList, intermediateList);
-                }
-                else if (currentLiteral.contains("*")){
+                } else if (currentLiteral.contains("*")) {
                     intermediateList = QueryProcessor.wildcardQuery(currentLiteral, posIndex, kgIndex);
                     masterList = ListMerge.intersectList(masterList, intermediateList);
-                }
-                else{
+                } else {
                     masterList = ListMerge.intersectList(masterList, posIndex.getPostingsList(currentLiteral));
                 }
             }
         }
         //Add this AND postings list to the collection of AND postings lists
-        AndCollection.add(masterList);       
+        AndCollection.add(masterList);
     }
-        
+
     //Run all AND Queries Q_i, store results in collection of positional 
     //postings lists, then run an OR query that merges all postings lists
     //into one master positional postings list representing the entire query.
-    public static List<PositionalPosting> orQuery(List<Subquery> allQueries, PositionalInvertedIndex posIndex,
-            KGramIndex kgIndex){
-       
+    public static List<PositionalPosting> orQuery
+        (List<Subquery> allQueries, PositionalInvertedIndex posIndex, KGramIndex kgIndex) {
+
         //Add all Q_i positional postings lists to AndCollection
-        for(int i = 0; i < allQueries.size(); i++){
+        for (int i = 0; i < allQueries.size(); i++) {
             addAndQuery(allQueries.get(i), posIndex, kgIndex);
         }
-        
+
         //Merge all Q_i postings list into Master List using OR intersection
         List<PositionalPosting> masterList = AndCollection.get(0);
-        
-        if(AndCollection.size() > 1){
-            for(int i = 1; i < AndCollection.size(); i++){
+
+        if (AndCollection.size() > 1) {
+            for (int i = 1; i < AndCollection.size(); i++) {
                 masterList = ListMerge.orList(masterList, AndCollection.get(i));
             }
         }
         return masterList;
     }
-    
+
     /**
      * Retrieve a list of positional postings that match the wildcard query
      *
@@ -114,11 +111,11 @@ public class QueryProcessor {
                 wcKGrams.add(fragment); // 1-,2-, or 3-grams
             }
         }
-        
+
         // Convert treeset to array to make iterating easier
         String[] kgrams = new String[wcKGrams.size()];
         kgrams = wcKGrams.toArray(kgrams);
-        
+
         // Merge the postings list of each k-gram
         List<String> candidates = new ArrayList<String>();
         if (kGramIndex.getPostingsList(kgrams[0]) != null) {
@@ -133,7 +130,7 @@ public class QueryProcessor {
         } else { // return if no matches
             return results;
         }
-        
+
         // Remove candidates that do not match the original query
         Iterator<String> iter = candidates.iterator();
         while (iter.hasNext()) {
@@ -141,7 +138,7 @@ public class QueryProcessor {
                 iter.remove();
             }
         }
-        
+
         // OR together the postings for the processed/stemmed term from each candidate    
         for (String candidate : candidates) { // will skip if candidates is empty
             // process and stem the token
@@ -156,20 +153,21 @@ public class QueryProcessor {
         }
         return results;
     }
-    
+
     /**
      * Retrieve a list of document IDs that match the author query
-     * 
+     *
      * @param aQuery query for the author's name
      * @param sIndex soundex index
      * @return list of document IDs that match the author's name
      */
-    public static List<Integer> authorQuery(String aQuery, SoundexIndex sIndex){
+    public static List<Integer> authorQuery(String aQuery, SoundexIndex sIndex) {
         List<Integer> result = new ArrayList<Integer>();
         TokenProcessorStream t = new TokenProcessorStream(aQuery);
-        while(t.hasNextToken()){
-            if(sIndex.getPostingsList(t.nextToken()) != null){
-                result = ListMerge.orList(result, sIndex.getPostingsList(t.nextToken()));
+        while (t.hasNextToken()) {
+            String name = t.nextToken();
+            if (sIndex.getPostingsList(name) != null) {
+                result = ListMerge.orList(result, sIndex.getPostingsList(name));
             }
         }
         return result;
