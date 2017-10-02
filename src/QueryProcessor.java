d7268ef7e6581d9344d15bd756a47161536dcdc6
Sylvia <Sylvia@XPS>
2017-10-02 10:02:55 -0700

    Move merging algorithms to query processor

--- src/QueryProcessor.java
+++ src/QueryProcessor.java
@@ -7,16 +7,16 @@ import java.util.TreeSet;
 
 /**
  * Class to process various types of queries
- * 
+ *
  */
 public class QueryProcessor {
 
     private static List<List<PositionalPosting>> AndCollection = new ArrayList<List<PositionalPosting>>();
 
     /**
-     * Add the positional postings list of an AND query to the 
-     * collection of AND query positional postings lists.
-     * 
+     * Add the positional postings list of an AND query to the collection of AND
+     * query positional postings lists.
+     *
      * @param andQueryLiterals
      * @param posIndex Positional inverted index of selected corpus
      * @param kgIndex KGram index of all types in corpus
@@ -34,10 +34,8 @@ public class QueryProcessor {
             masterList = QueryProcessor.wildcardQuery(preLiteral, posIndex, kgIndex);
         } else if (preLiteral.contains("near")) {
             masterList = nearQuery(preLiteral, posIndex);
-        } else {
-            if (posIndex.getPostingsList(preLiteral) != null){
-                masterList = posIndex.getPostingsList(preLiteral);
-            }
+        } else if (posIndex.getPostingsList(preLiteral) != null) {
+            masterList = posIndex.getPostingsList(preLiteral);
         }
 
         /* Merge all of the postings lists of each query literal
@@ -50,47 +48,42 @@ public class QueryProcessor {
                 if (currentLiteral.contains("\"")) {
                     intermediateList = phraseQuery(currentLiteral, posIndex);
                     if (masterList != null && intermediateList != null) {
-                        masterList = ListMerge.intersectList(masterList, intermediateList);
+                        masterList = intersectList(masterList, intermediateList);
                     }
                 } else if (currentLiteral.contains("*")) {
                     intermediateList = QueryProcessor.wildcardQuery(currentLiteral, posIndex, kgIndex);
-                    masterList = ListMerge.intersectList(masterList, intermediateList);
+                    masterList = intersectList(masterList, intermediateList);
                 } else if (currentLiteral.contains("near")) {
-                    intermediateList = nearQuery(currentLiteral, posIndex); 
-                    masterList = ListMerge.intersectList(masterList, intermediateList);
+                    intermediateList = nearQuery(currentLiteral, posIndex);
+                    masterList = intersectList(masterList, intermediateList);
+                } else if (posIndex.getPostingsList(currentLiteral) != null) {
+                    masterList = intersectList(masterList, posIndex.getPostingsList(currentLiteral));
                 } else {
-                    if (posIndex.getPostingsList(currentLiteral) != null) {
-                        masterList = ListMerge.intersectList(masterList, posIndex.getPostingsList(currentLiteral));
-                    }
-                    else {
-                        masterList.clear();
-                    }
+                    masterList.clear();
                 }
             }
         }
         //Add this AND postings list to the collection of AND postings lists
-        if (masterList != null){
+        if (masterList != null) {
             AndCollection.add(masterList);
-        }
-        else {
+        } else {
             AndCollection.add(masterList);
             masterList.clear();
-        } 
+        }
     }
 
     /**
-     * Run all AND Queries Q_i, store results in collection of positional 
-     * postings lists, then run an OR query that merges all postings lists
-     * into one master positional postings list representing the entire query.
-    
+     * Run all AND Queries Q_i, store results in collection of positional
+     * postings lists, then run an OR query that merges all postings lists into
+     * one master positional postings list representing the entire query.
+     *
      * @param allQueries List of subqueries that represents user query
      * @param posIndex Positional inverted index of selected corpus
      * @param kgIndex KGram index of all types in corpus
-     * @return Positional Posting list representing all AND queries
-     * merged together using OR logic.
+     * @return Positional Posting list representing all AND queries merged
+     * together using OR logic.
      */
-    public static List<PositionalPosting> orQuery
-        (List<Subquery> allQueries, PositionalInvertedIndex posIndex, KGramIndex kgIndex) {
+    public static List<PositionalPosting> orQuery(List<Subquery> allQueries, PositionalInvertedIndex posIndex, KGramIndex kgIndex) {
 
         //Add all Q_i positional postings lists to AndCollection
         for (int i = 0; i < allQueries.size(); i++) {
@@ -102,7 +95,7 @@ public class QueryProcessor {
 
         if (AndCollection.size() > 1) {
             for (int i = 1; i < AndCollection.size(); i++) {
-                masterList = ListMerge.orList(masterList, AndCollection.get(i));
+                masterList = unionList(masterList, AndCollection.get(i));
             }
         }
         AndCollection.clear();
@@ -117,8 +110,7 @@ public class QueryProcessor {
      * @param kGramIndex K-Gram index of the vocabulary type
      * @return list of resulting Positional Postings
      */
-    public static List<PositionalPosting> wildcardQuery
-        (String wcQuery, PositionalInvertedIndex index, KGramIndex kGramIndex) {
+    public static List<PositionalPosting> wildcardQuery(String wcQuery, PositionalInvertedIndex index, KGramIndex kGramIndex) {
 
         // Generate all the k-grams for the wildcard
         SortedSet<String> wcKGrams = new TreeSet<String>();
@@ -156,7 +148,7 @@ public class QueryProcessor {
             candidates = kGramIndex.getPostingsList(kgrams[0]);
             for (int i = 1; i < kgrams.length; i++) {
                 if (kGramIndex.getPostingsList(kgrams[i]) != null) {
-                    candidates = ListMerge.intersectList(candidates, kGramIndex.getPostingsList(kgrams[i]));
+                    candidates = intersectList(candidates, kGramIndex.getPostingsList(kgrams[i]));
                 } else { // return if no matches
                     return results;
                 }
@@ -180,7 +172,7 @@ public class QueryProcessor {
             while (t.hasNextToken()) {
                 String term = PorterStemmer.getStem(t.nextToken());
                 if (index.getPostingsList(term) != null) {
-                    results = ListMerge.orList(results, index.getPostingsList(term));
+                    results = unionList(results, index.getPostingsList(term));
                 }
 
             }
@@ -189,25 +181,6 @@ public class QueryProcessor {
     }
 
     /**
-     * Retrieve a list of document IDs that match the author query
-     *
-     * @param aQuery query for the author's name
-     * @param sIndex soundex index
-     * @return list of document IDs that match the author's name
-     */
-    public static List<Integer> authorQuery(String aQuery, SoundexIndex sIndex) {
-        List<Integer> result = new ArrayList<Integer>();
-        TokenProcessorStream t = new TokenProcessorStream(aQuery);
-        while (t.hasNextToken()) {
-            String name = t.nextToken();
-            if (sIndex.getPostingsList(name) != null) {
-                result = ListMerge.orList(result, sIndex.getPostingsList(name));
-            }
-        }
-        return result;
-    }
-    
-     /**
      * Uses positional intersection algorithm to merge all phrase terms into one
      * final list representing the results for the entire phrase.
      *
@@ -224,18 +197,17 @@ public class QueryProcessor {
         for (int i = 0; i < spPhrase.length; i++) {
             spPhrase[i] = phraseStemmer.getStem(spPhrase[i]);
         }
-  
-        if (posIndex.getPostingsList(spPhrase[0]) != null){
+
+        if (posIndex.getPostingsList(spPhrase[0]) != null) {
             phraseList = posIndex.getPostingsList(spPhrase[0]);
-            for(int j = 1; j < spPhrase.length; j++){
-                if(posIndex.getPostingsList(spPhrase[j]) != null) {
-                    phraseList = ListMerge.positionalIntersect(phraseList,
-                    posIndex.getPostingsList(spPhrase[j]), 1);
+            for (int j = 1; j < spPhrase.length; j++) {
+                if (posIndex.getPostingsList(spPhrase[j]) != null) {
+                    phraseList = positionalIntersect(phraseList,
+                            posIndex.getPostingsList(spPhrase[j]), 1);
                 } else { // return empty list
                     phraseList.clear();
                     return phraseList;
-                    
-                    
+
                 }
             }
         } else { // return empty list 
@@ -260,10 +232,178 @@ public class QueryProcessor {
         int k = Integer.valueOf(spNear[1].substring(4));
 
         if (posIndex.getPostingsList(spNear[0]) != null && posIndex.getPostingsList(spNear[2]) != null) {
-            nearList = ListMerge.positionalIntersect(posIndex.getPostingsList(spNear[0]),
+            nearList = positionalIntersect(posIndex.getPostingsList(spNear[0]),
                     posIndex.getPostingsList(spNear[2]), k);
         }
         return nearList;
     }
-    
+
+    /**
+     * Retrieve a list of document IDs that match the author query
+     *
+     * @param aQuery query for the author's name
+     * @param sIndex soundex index
+     * @return list of document IDs that match the author's name
+     */
+    public static List<Integer> authorQuery(String aQuery, SoundexIndex sIndex) {
+        List<Integer> result = new ArrayList<Integer>();
+        TokenProcessorStream t = new TokenProcessorStream(aQuery);
+        while (t.hasNextToken()) {
+            String name = t.nextToken();
+            if (sIndex.getPostingsList(name) != null) {
+                result = unionList(result, sIndex.getPostingsList(name));
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Get the intersection of two ordered lists
+     *
+     * @param <T>
+     * @param list1 list of comparable elements
+     * @param list2 list of comparable elements
+     * @return resulting list of intersecting list1 and list2
+     */
+    public static <T extends Comparable> List<T> intersectList(List<T> list1, List<T> list2) {
+        List<T> result = new ArrayList<T>();
+        int i = 0;
+        int j = 0;
+
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i).compareTo(list2.get(j)) == 0) {
+                result.add(list1.get(i));
+                i++;
+                j++;
+            } else if (list1.get(i).compareTo(list2.get(j)) < 0) { // list1 before list2
+                i++;
+            } else { // list2 before list1
+                j++;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Get the union of two ordered lists
+     *
+     * @param <T>
+     * @param list1 list of comparable elements
+     * @param list2 list of comparable elements
+     * @return resulting list of the union of list1 and list2
+     */
+    public static <T extends Comparable> List<T> unionList(List<T> list1, List<T> list2) {
+        List<T> result = new ArrayList<T>();
+        int i = 0;
+        int j = 0;
+
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i).compareTo(list2.get(j)) == 0) {
+                result.add(list1.get(i));
+                i++;
+                j++;
+            } else if (list1.get(i).compareTo(list2.get(j)) < 0) { // list1 before list2 
+                result.add(list1.get(i));
+                i++;
+            } else { // list2 before list1
+                result.add(list2.get(j));
+                j++;
+            }
+        }
+
+        // append the longer list to the results
+        if (i < list1.size()) {
+            while (i < list1.size()) {
+                result.add(list1.get(i));
+                i++;
+            }
+        } else if (j < list2.size()) {
+            while (j < list2.size()) {
+                result.add(list2.get(j));
+                j++;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Positional intersection of two terms where the second term appears within
+     * k positions after the first. Source: Introduction to Information
+     * Retrieval (Figure 2.12)
+     *
+     * @param term1 positional postings list of the first term
+     * @param term2 positional postings list of the second term
+     * @param k max positions that term2 appears after term1
+     * @return positional postings list from the intersection; the position
+     * corresponds to term2
+     */
+    public static List<PositionalPosting> positionalIntersect(List<PositionalPosting> term1, List<PositionalPosting> term2, int k) {
+
+        List<PositionalPosting> result = new ArrayList<PositionalPosting>();
+        List<Integer> docs1 = new ArrayList<Integer>(); // term1 documents
+        List<Integer> docs2 = new ArrayList<Integer>(); //term2 documents
+        int i = 0; // term1 document index
+        int j = 0; // term2 document index
+
+        // load the docIDs to list
+        for (PositionalPosting p : term1) {
+            docs1.add(p.getDocumentID());
+        }
+        for (PositionalPosting p : term2) {
+            docs2.add(p.getDocumentID());
+        }
+
+        // intersect the docs
+        while (i < docs1.size() && j < docs2.size()) {
+            // both terms appear in the doc
+            if ((int) docs1.get(i) == docs2.get(j)) {
+                List<Integer> candidate = new ArrayList<Integer>();
+                List<Integer> pp1 = term1.get(i).getTermPositions(); // term1 positions
+                List<Integer> pp2 = term2.get(j).getTermPositions(); // term2 positions
+                int ii = 0; // term1 position index
+                int jj = 0; // term2 position index
+
+                // check if term2 appears within k positions after term1
+                while (ii < pp1.size()) {
+                    while (jj < pp2.size()) {
+                        int relativePos = pp2.get(jj) - pp1.get(ii);
+                        if (relativePos > 0 && relativePos <= k) {
+                            // add term2 position to candidates
+                            candidate.add(pp2.get(jj));
+                        } else if (pp2.get(jj) > pp1.get(ii)) {
+                            break;
+                        }
+                        jj++;
+                    }
+                    // remove duplicate matches
+                    while (!candidate.isEmpty() && Math.abs(candidate.get(0) - pp1.get(ii)) > k) {
+                        candidate.remove(0);
+                    }
+                    // add candidates to the result 
+                    for (Integer pos : candidate) {
+                        int currentIndex = result.size() - 1;
+                        if (!result.isEmpty() && result.get(currentIndex).getDocumentID() == docs1.get(i)) {
+                            // the query appears more than once in the doc
+                            // add the position to existing posting
+                            result.get(currentIndex).addPosition(pos);
+                        } else { // add a new posting to the result 
+                            result.add(new PositionalPosting(docs1.get(i), pos));
+                        }
+                    }
+                    ii++;
+                }
+                i++;
+                j++;
+            } else if (docs1.get(i) < docs2.get(j)) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return result;
+    }
+
 }
