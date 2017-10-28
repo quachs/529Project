3e5474be3a253a20f178031f5584e33547b6e9f5
Sandra Kramlich <sandra.kramlich@t-online.de>
2017-10-28 13:12:48 -0700

    Added interface ThreadFinishedCallBack
    Informs UI when the Thread is finished

--- src/Indexes/IndexingGUI.java
+++ src/Indexes/IndexingGUI.java
@@ -1,16 +1,19 @@
 package Indexes;
 
+import Threads.Indexing;
 import Helper.ProgressDialog;
+import Threads.ThreadFinishedCallBack;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.swing.*;
 
-public class IndexingGUI extends JFrame{
+public class IndexingGUI extends JFrame implements ThreadFinishedCallBack{
 private Path path; // for saving the path// background tasks
     private Indexing indexedCorpus = new Indexing(); // task for indexing
     
     private ProgressDialog progress;
     private ImageIcon img = new ImageIcon(System.getProperty("user.dir") + "/icon.png"); // logo icon
+    private Thread thread;
     
     public IndexingGUI() {
         path = Paths.get("");
@@ -48,12 +51,17 @@ private Path path; // for saving the path// background tasks
      * While Indexing processes show a progress bar
      */
     private void indexing() {
-        progress.setVisible(true); // show the dialog        
-        indexedCorpus = new Indexing(path); // start the task idexing
-        indexedCorpus.execute(); // start the task  
-        // wait till the task is finished -> set true when done() is finished
-        while (!indexedCorpus.isDone()) {
-        }        
+        progress.setVisible(true); // show the dialog   
+        indexedCorpus = new Indexing(path, this); // start the task idexing  
+        thread = new Thread(indexedCorpus);  
+        thread.start(); // start the task  
+    }
+
+    /**
+     * This is called when the task is finished.
+     */
+    @Override
+    public void notifyThreadFinished() {
         progress.setVisible(false); // close the dialog
         JOptionPane.showMessageDialog(this, "Indexing is finished.",
                             "Result of stemming", JOptionPane.INFORMATION_MESSAGE, this.img);
