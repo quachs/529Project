a7227991634daf15d156486143bd5d50424900c9
Sandra Kramlich <sandra.kramlich@t-online.de>
2017-10-30 00:53:13 -0700

    Add File Chooser to query parsing option

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -2,7 +2,10 @@
 import Helper.Formulars;
 import Indexes.IndexingGUI;
 import Retrivals.RetrievalGUI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
@@ -15,7 +18,9 @@ import javax.swing.UnsupportedLookAndFeelException;
  */
 public class UserInterface extends JFrame {
 
+    private Path path = Paths.get("");
     private ImageIcon img = new ImageIcon(System.getProperty("user.dir") + "/icon.png"); // logo icon
+
     public UserInterface() {
         // Change look and feel of swing components
         // resource: http://docs.oracle.com/javase/tutorial/uiswing/lookandfeel/nimbus.html
@@ -41,9 +46,11 @@ public class UserInterface extends JFrame {
                 optionsBeginning,
                 optionsBeginning[1]);
         if (resultBeginning == 0) {
-            new IndexingGUI();
+            path = chooseDirectory();
+            new IndexingGUI(path);
         } else //Custom button text
         {
+            path = chooseDirectory();            
             Object[] optionsRetrival = {"Boolean Retrival",
                 "Ranked Retrieval"};
             int resultRetrival = JOptionPane.showOptionDialog(this,
@@ -55,20 +62,47 @@ public class UserInterface extends JFrame {
                     optionsRetrival,
                     optionsRetrival[1]);
             if (resultRetrival == 0) {
-                new RetrievalGUI('b', null);
+                new RetrievalGUI('b', null, path.toString());
             } else {
                 Object[] optionsForm = {"Default",
-                "tf-idf","Okapi BM25","Wacky"};
+                    "tf-idf", "Okapi BM25", "Wacky"};
                 int resultFormular = JOptionPane.showOptionDialog(this,
-                    "Please choose the type of formular",
-                    "Formular",
-                    JOptionPane.YES_NO_CANCEL_OPTION,
-                    JOptionPane.PLAIN_MESSAGE,
-                    img,
-                    optionsForm,
-                    optionsForm[1]);
-                new RetrievalGUI('r', Formulars.getFormByID(resultFormular));
+                        "Please choose the type of formular",
+                        "Formular",
+                        JOptionPane.YES_NO_CANCEL_OPTION,
+                        JOptionPane.PLAIN_MESSAGE,
+                        img,
+                        optionsForm,
+                        optionsForm[1]);
+                new RetrievalGUI('r', Formulars.getFormByID(resultFormular), path.toString());
             }
+        }
+    }
+
+    /**
+     * Open a file chooser dialog for choosing the directory the user wants to
+     * index
+     */
+    private Path chooseDirectory() {
+        JFileChooser chooser = new JFileChooser(); // initialize file chooser        
+        chooser.setCurrentDirectory(new java.io.File(path.toString())); // start with the saved path        
+        chooser.setDialogTitle("Choose Directory for corpus"); // set the title of the dialog
+
+        // tell the file chooser that it only shows the directory that the user is 
+        // only able to choose a directory and not a file
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        // resChooser saves result of dialog: int for JFileChooser options (APPROVE_OPTION, ABORT,...)
+        // chooser.showOpenDialog(null) opens the dialog in the 
+        // middle of the screen, it is not depended by any frame or component
+        int resChooser = chooser.showOpenDialog(null);
+
+        // if user approves -> clicks "Open" ...
+        if (JFileChooser.APPROVE_OPTION == resChooser) {
+            return Paths.get(chooser.getSelectedFile().getPath()); // save the returned path,
+        } else {
+            System.exit(0); // Close the system if user presses x or cancel
+            return null;
         }
     }
 }
