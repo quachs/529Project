576dca9bbc28632d0a476e717ec1efbc530245b4
Sandra Kramlich <sandra.kramlich@t-online.de>
2017-11-10 00:22:07 -0800

    Fixed part of calculation issue
    Now we are saving in the hash map diskPostings instead of only the docId because we need the term frequency for calculating the L_d of okapi algorithm.
    The results are still not correct but they are more equal. We need to find the other problems. I just let in all my user outputs, maybe they help you finding some issues.

--- src/formulas/OkapiForm.java
+++ src/formulas/OkapiForm.java
@@ -24,17 +24,36 @@ public class OkapiForm extends Formular {
 
     @Override
     public double calcWDT(DiskPosting dPosting) {
-        return 2.2 * dPosting.getTermFrequency();
+        if(dIndex.getFileNames().get(dPosting.getDocumentID()).equals("769.json")){
+            System.out.println("OK: json TermFrequency-WDT for "+dPosting.getDocumentID()+": "+dPosting.getTermFrequency());
+        }
+        if(dPosting.getDocumentID() == 744){
+            System.out.println("OK: 744 TermFrequency-WDT for "+dPosting.getDocumentID()+": "+dPosting.getTermFrequency());
+        }
+        return (2.2 * dPosting.getTermFrequency());
     }
 
     @Override
     public double getL_D(int docID) {
-        return (1.2 * (0.25 + 0.75 * (dIndex.getDocLength(docID) / dIndex.getAvgDocLength()))) + dPosting.getTermFrequency();
+        if(dIndex.getFileNames().get(docID).equals("769.json")){
+            System.out.println("OK: json DocLengh for "+dPosting.getDocumentID()+": "+dIndex.getDocLength(docID));
+            System.out.println("OK: json AverageDoc for "+dPosting.getDocumentID()+": "+dIndex.getAvgDocLength());
+            System.out.println("OK: json TermFrequency-LD for "+dPosting.getDocumentID()+": "+dPosting.getTermFrequency());
+        }
+        if(docID==744){
+            System.out.println("OK: 744 DocLengh for "+dPosting.getDocumentID()+": "+dIndex.getDocLength(docID));
+            System.out.println("OK: 744 AverageDoc for "+dPosting.getDocumentID()+": "+dIndex.getAvgDocLength());
+            System.out.println("OK: 744 TermFrequency-LD for "+dPosting.getDocumentID()+": "+dPosting.getTermFrequency());
+        }
+        return (1.2 * (0.25 + 0.75 * (((double)dIndex.getDocLength(docID)) / ((double)dIndex.getAvgDocLength())))) + dPosting.getTermFrequency();
     }
 
     public void setdPosting(DiskPosting dPosting) {
         this.dPosting = dPosting;
     }
 
-    
+    public void testDiskIndex(){
+        System.out.println("DiskIndex 744: "+dIndex.getFileNames().get(744));
+        System.out.println("DiskIndex 999: "+dIndex.getFileNames().get(999));
+    }
 }
--- src/retrivals/rankedRetrival/RankedRetrieval.java
+++ src/retrivals/rankedRetrival/RankedRetrieval.java
@@ -43,22 +43,28 @@ public class RankedRetrieval {
     }
 
     private double calcWQT(List<DiskPosting> tDocIDs) {
-        System.out.println("FormEnum: "+formEnum);
+        System.out.println("FormEnum: " + formEnum);
         double t = form.calcWQT(tDocIDs);
-        System.out.println("WQT: "+t);
+        System.out.println("RR: WQT: " + t);
         return t;
     }
 
     //Adapted from Sylvia's IndexWriter.buildWeightFile;
     private double calcWDT(DiskPosting dPosting) {
+        if (dIndex.getFileNames().get(dPosting.getDocumentID()).equals("769.json")) {
+            System.out.println("RR: WDT for " + dPosting.getDocumentID() + ": " + form.calcWDT(dPosting));
+        }
         return form.calcWDT(dPosting);
     }
 
     private double getL_D(int docID) {
+        if (dIndex.getFileNames().get(docID).equals("769.json")) {
+            System.out.println("RR: L_D for " + docID + ": " + form.getL_D(docID));
+        }
         return form.getL_D(docID);
     }
 
-    private static void accumulate(HashMap<Integer, Double> acc, int docID, double A_d) {
+    private static void accumulate(HashMap<DiskPosting, Double> acc, DiskPosting docID, double A_d) {
 
         if (acc.containsKey(docID)) {
             double newA_d = acc.get(docID) + A_d;
@@ -70,12 +76,12 @@ public class RankedRetrieval {
 
     public RankedItem[] rankedQuery(KGramIndex kIndex, Subquery query, int k) {
         List<DiskPosting> dPostings = null;
-        HashMap<Integer, Double> acc = new HashMap<Integer, Double>();
+        HashMap<DiskPosting, Double> acc = new HashMap<DiskPosting, Double>();
         PriorityQueue<RankedItem> A_dQueue = new PriorityQueue<RankedItem>();
         List<RankedItem> returnedRIs = new ArrayList<RankedItem>();
 
         for (String queryLit : query.getLiterals()) {
-            System.out.println("Literal: " +queryLit);
+            System.out.println("Literal: " + queryLit);
             //Collect A_d values for each document, add to priority queue
             if (queryLit.contains("*")) {
                 List<DiskPosting> wcResults = DiskQueryProcessor.wildcardQuery(queryLit, dIndex, kIndex);
@@ -92,28 +98,27 @@ public class RankedRetrieval {
 
                 for (DiskPosting dPosting : dPostings) {
                     double newAccumulator = calcWDT(dPosting) * WQT;
-                    accumulate(acc, dPosting.getDocumentID(), newAccumulator);
+                    accumulate(acc, dPosting, newAccumulator);
+                    if (dIndex.getFileNames().get(dPosting.getDocumentID()).equals("769.json")) {
+                        System.out.println("RR: Accumulator for " + dPosting.getDocumentID() + ": " + acc.get(dPosting));
+                    }
                 }
             }
         }
         this.sizeOfFoundDocs = acc.size();
-        Integer[] relevantDocuments = new Integer[acc.size()];
+        DiskPosting[] relevantDocuments = new DiskPosting[acc.size()];
         acc.keySet().toArray(relevantDocuments);
 
-        for (int relevantDocument : relevantDocuments) {
+        for (DiskPosting relevantDocument : relevantDocuments) {
 
             double accumulator = acc.get(relevantDocument);
             if (accumulator > 0.0) {
                 if (form instanceof OkapiForm) {
-                    for (DiskPosting dPosting : dPostings) {
-                        if (acc.containsKey(dPosting.getDocumentID())) {
-                            ((OkapiForm) form).setdPosting(dPosting);
-                        }
-                    }
+                    ((OkapiForm) form).setdPosting(relevantDocument);
                 }
-                double docWeight = getL_D(relevantDocument);
+                double docWeight = getL_D(relevantDocument.getDocumentID());
                 double rank = accumulator / docWeight;
-                A_dQueue.add(new RankedItem(rank, relevantDocument));
+                A_dQueue.add(new RankedItem(rank, relevantDocument.getDocumentID()));
             }
         }
 
@@ -137,4 +142,8 @@ public class RankedRetrieval {
         return sizeOfFoundDocs;
     }
 
+    public void testDiskIndex() {
+        System.out.println("DiskIndex 744: " + dIndex.getFileNames().get(744));
+        System.out.println("DiskIndex 999: " + dIndex.getFileNames().get(999));
+    }
 }
