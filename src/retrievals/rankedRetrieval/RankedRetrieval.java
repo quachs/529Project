c520a0be016f25bcd41ec0ad0686435e371ac2b8
svidstu <Sean.Vidal@student.csulb.edu>
2017-11-11 21:46:20 -0800

    Added javadoc to RankedRetrieval

--- src/retrievals/rankedRetrieval/RankedRetrieval.java
+++ src/retrievals/rankedRetrieval/RankedRetrieval.java
@@ -16,6 +16,13 @@ import java.lang.*;
 
 //https://docs.oracle.com/javase/7/docs/api/java/lang/Double.html
 //https://docs.oracle.com/javase/8/docs/api/java/util/Scanner.html
+
+/**
+ * Class used to process ranked queries;
+ * follows term-at-a-time document scoring
+ * as presented during CECS 429/529 lecture.
+ * 
+ */
 public class RankedRetrieval {
 
     private DiskInvertedIndex dIndex;
@@ -49,7 +56,12 @@ public class RankedRetrieval {
         return t;
     }
 
-    // Adapted from Sylvia's IndexWriter.buildWeightFile;
+    /**
+     * Use selected idf formula to calculate WDT value
+     * 
+     * @param dPosting
+     * @return WDT value for the corresponding document
+     */
     private double calcWDT(DiskPosting dPosting) {
         if (dIndex.getFileNames().get(dPosting.getDocumentID()).equals("769.json")) {
             System.out.println("RR: WDT for " + dPosting.getDocumentID() + ": " + form.calcWDT(dPosting));
@@ -57,6 +69,12 @@ public class RankedRetrieval {
         return form.calcWDT(dPosting);
     }
 
+    /**
+     * Retrieve the normalized length value for the selected document
+     * 
+     * @param docID
+     * @return L_D, which represents the normalized document length
+     */ 
     private double getL_D(int docID) {
         if (dIndex.getFileNames().get(docID).equals("769.json")) {
             System.out.println("RR: L_D for " + docID + ": " + form.getL_D(docID));
@@ -68,9 +86,9 @@ public class RankedRetrieval {
      * Term-at-a-time scoring algorithm
      * as presented during lecture
      * 
-     * @param accumulators
-     * @param docID
-     * @param accDocScore 
+     * @param accumulators Hashmap associating a document with its accumulated score
+     * @param docID A document which may or may not have a non-zero accumulated score
+     * @param accDocScore Accumulated document score associated with docID
      */
     private static void accumulate(HashMap<DiskPosting, Double> accumulators, DiskPosting docID, double accDocScore) {
 
@@ -82,6 +100,15 @@ public class RankedRetrieval {
         }
     }
 
+    /**
+     * Term-at-a-time document scoring method.
+     * 
+     * @param kIndex KGramIndex used to process a wildcard query
+     * @param query User input stored in Subquery structure
+     * @param k Number of relevant documents to return in the result set
+     * @return Array of top k relevant documents and their scores,
+     * sorted by most relevant to least relevant
+     */
     public RankedDocument[] rankedQuery(KGramIndex kIndex, Subquery query, int k) {
         List<DiskPosting> dPostings = null;
         HashMap<DiskPosting, Double> accumulators = new HashMap<DiskPosting, Double>();
@@ -90,6 +117,7 @@ public class RankedRetrieval {
 
         for (String queryLit : query.getLiterals()) {
             System.out.println("Literal: " + queryLit);
+            
             // Collect accDocScore values for each document, add to priority queue
             if (queryLit.contains("*")) {
                 List<DiskPosting> wcResults = DiskQueryProcessor.wildcardQuery(queryLit, dIndex, kIndex);
@@ -107,23 +135,34 @@ public class RankedRetrieval {
                 for (DiskPosting dPosting : dPostings) {
                     double newAccumulator = calcWDT(dPosting) * WQT;
                     accumulate(accumulators, dPosting, newAccumulator);
+                    
                     if (dIndex.getFileNames().get(dPosting.getDocumentID()).equals("769.json")) {
                         System.out.println("RR: Accumulator for " + dPosting.getDocumentID() + ": " + accumulators.get(dPosting));
                     }
                 }
             }
         }
+        
         this.sizeOfFoundDocs = accumulators.size();
+        
+        
+        // Create an array of all relevant documents.
+        // Relevancy is determine by its membership
+        // in the hashmap of accumulators
         DiskPosting[] relevantDocuments = new DiskPosting[accumulators.size()];
         accumulators.keySet().toArray(relevantDocuments);
 
+        // Add all normalized, non-zero document scores to priority queue
         for (DiskPosting relevantDocument : relevantDocuments) {
 
             double accumulator = accumulators.get(relevantDocument);
+            
             if (accumulator > 0.0) {
+                
                 if (form instanceof OkapiForm) {
                     ((OkapiForm) form).setdPosting(relevantDocument);
                 }
+                
                 double docWeight = getL_D(relevantDocument.getDocumentID());
                 double rank = accumulator / docWeight;
                 rankedQueue.add(new RankedDocument(rank, relevantDocument.getDocumentID()));
@@ -140,11 +179,13 @@ public class RankedRetrieval {
             k = accumulators.size();
         }
         
+        // Add the top k documents to the final result set.
         for (int i = 0; i < k; i++) {
             RankedDocument ri = rankedQueue.poll();
             returnedRIs.add(ri);
         }
 
+        // Convert result set to array
         RankedDocument[] results = new RankedDocument[returnedRIs.size()];
         returnedRIs.toArray(results);
         return results;
