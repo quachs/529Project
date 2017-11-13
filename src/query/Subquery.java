2d804b0746a38f34fe25bd3c3eae91e11eb1e5c8
svidstu <Sean.Vidal@student.csulb.edu>
2017-11-12 22:05:10 -0800

    Added comments to subquery

--- src/query/Subquery.java
+++ src/query/Subquery.java
@@ -1,10 +1,14 @@
 package query;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A class that maintains all of an AND query literals
+ * in a list.  Additionally, this class provides methods
+ * to add literals to the list and retrieve all literals.
+ * 
+ */
 public class Subquery {
     private List<String> literals;
     
