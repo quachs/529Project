3577e5f4bb0dacee0d39d6ad87f1785c94e530b2
Sylvia Quach <14224414+quachs@users.noreply.github.com>
2017-12-08 18:16:44 -0800

    Index the disputed documents and classify

--- src/classification/BayesianClassification.java
+++ src/classification/BayesianClassification.java
@@ -16,68 +16,134 @@ import java.util.PriorityQueue;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-public class BayesTraining {
+public class BayesianClassification {
 
+    private final List<String> discSet; // discriminating set
     private final List<TrainingDocument> trainingDocs;
     private final SortedSet<String> trainingTerms;
-    private final List<String> discSet; // discriminating set
-    private final List<Double> hamiltonTermProb; 
-    private final List<Double> madisonTermProb; 
-    private final List<Double> jayTermProb; 
-    private int hamiltonCount; 
-    private int madisonCount; 
+    private final List<Double> hamiltonTermProb;
+    private final List<Double> jayTermProb;
+    private final List<Double> madisonTermProb;
+    private int hamiltonCount;
     private int jayCount;
+    private int madisonCount;
+    private final NaiveInvertedIndex disputedIndex;
+    private final List<String> disputedFileNames;
 
-    
-    
-    public BayesTraining(String path, int k) {
-        
+    public BayesianClassification(String path, int k) {
+
         // Build the discriminating set
         trainingDocs = new ArrayList<TrainingDocument>();
         trainingTerms = new TreeSet<String>();
-        NaiveInvertedIndex hIndex = new NaiveInvertedIndex(); // Hamilton index
-        NaiveInvertedIndex mIndex = new NaiveInvertedIndex(); // Madison index
-        NaiveInvertedIndex jIndex = new NaiveInvertedIndex(); // Jay index
+        NaiveInvertedIndex hamiltonIndex = new NaiveInvertedIndex();
+        NaiveInvertedIndex jayIndex = new NaiveInvertedIndex();
+        NaiveInvertedIndex madisonIndex = new NaiveInvertedIndex();
         hamiltonCount = 0;
-        madisonCount = 0;
         jayCount = 0;
-        indexFile(Paths.get(path + "\\HAMILTON"), hIndex);
-        indexFile(Paths.get(path + "\\MADISON"), mIndex);
-        indexFile(Paths.get(path + "\\JAY"), jIndex);
+        madisonCount = 0;
+        indexFile(Paths.get(path + "\\HAMILTON"), hamiltonIndex);
+        indexFile(Paths.get(path + "\\JAY"), jayIndex);
+        indexFile(Paths.get(path + "\\MADISON"), madisonIndex);
         discSet = getDiscSet(k);
-        
+
         // Save the term probabilities for each class
         hamiltonTermProb = new ArrayList<Double>(k);
         madisonTermProb = new ArrayList<Double>(k);
         jayTermProb = new ArrayList<Double>(k);
-        calcTermProb(discSet, hIndex, hamiltonTermProb);
-        calcTermProb(discSet, mIndex, madisonTermProb);
-        calcTermProb(discSet, jIndex, jayTermProb);
+        calcTermProb(discSet, hamiltonIndex, hamiltonTermProb);
+        calcTermProb(discSet, madisonIndex, madisonTermProb);
+        calcTermProb(discSet, jayIndex, jayTermProb);
+
+        // Index the disputed documents
+        disputedFileNames = new ArrayList<String>();
+        disputedIndex = new NaiveInvertedIndex();
+        indexFile(Paths.get(path + "\\DISPUTED"), disputedIndex);
         
+    }
+
+    public void runClassifier() {
+
+        double n = hamiltonCount + madisonCount + jayCount; // training set count
+
+        for (int docId = 0; docId < disputedFileNames.size(); docId++) {
+
+            // Probability of the doc being in the class
+            double hamiltonProb = Math.log(hamiltonCount / n);
+            double jayProb = Math.log(jayCount / n);
+            double madisonProb = Math.log(madisonCount / n);
+
+            for (int termIndex = 0; termIndex < discSet.size(); termIndex++) {
+                
+                // Add the log of term probability to the corresponding class
+                if (disputedIndex.getPostings(discSet.get(termIndex)) != null) {
+                    if (disputedIndex.getPostings(discSet.get(termIndex)).contains(docId)) {
+                        hamiltonProb += Math.log(hamiltonTermProb.get(termIndex));
+                        jayProb += Math.log(jayTermProb.get(termIndex));
+                        madisonProb += Math.log(madisonTermProb.get(termIndex));
+                    }
+                }
+            }
+
+            
+            System.out.print(disputedFileNames.get(docId)+ " -> ");
+            System.out.println(maxProb(new ClassProb(Authors.HAMILTON, hamiltonProb),
+                                    new ClassProb(Authors.JAY, jayProb), 
+                                    new ClassProb(Authors.MADISON, madisonProb)));
+           
+            System.out.println("Hamilton: " + hamiltonProb);
+            System.out.println("Jay: " + jayProb);
+            System.out.println("Madison: " + madisonProb);
+            System.out.println();
+        }
+    }
+
+    /**
+     * Return the class object with the higher probability
+     * @param a
+     * @param b
+     * @return 
+     */
+    private ClassProb maxProb(ClassProb a, ClassProb b) {
+        if(a.getProb() > b.getProb()){
+            return a;
+        }
+        return b;
+    }
+    
+    /**
+     * Return the class author with the highest probability
+     * @param a
+     * @param b
+     * @param c
+     * @return 
+     */
+    private Authors maxProb(ClassProb a, ClassProb b, ClassProb c) {
+        return maxProb(maxProb(a,b), c).getAuthor();
     }
 
     /**
      * From the discriminating set, save the term probabilities for a class
+     *
      * @param discSet the discriminating set
      * @param index of the class
      * @param termProb map of a term to its probability
      */
     private void calcTermProb(List<String> discSet, NaiveInvertedIndex index, List<Double> termProb) {
-        
+
         int totalTf = 0; // total number of ocurrences in the class
-        
-        // Save (tf + 1) for each term in the class
-        for(int i = 0; i < discSet.size(); i++){
+
+        // Save tf for each term in the class
+        for (int i = 0; i < discSet.size(); i++) {
             double tf = 0;
-            if(index.getPostings(discSet.get(i)) != null){
+            if (index.getPostings(discSet.get(i)) != null) {
                 tf = index.getPostings(discSet.get(i)).size();
                 totalTf += tf;
             }
-            termProb.add(tf + 1);
+            termProb.add(tf + 1); // add 1 for smoothing
         }
 
         // Divide to get the probability - update list
-        for(int i = 0; i < discSet.size(); i++){
+        for (int i = 0; i < discSet.size(); i++) {
             termProb.set(i, termProb.get(i) / (totalTf + discSet.size()));
         }
 
@@ -110,13 +176,11 @@ public class BayesTraining {
                         }
 
                     } else // not in the class
-                    {
-                        if (doc.getTerms().contains(term)) {
+                     if (doc.getTerms().contains(term)) {
                             n01++;
                         } else {
                             n00++;
                         }
-                    }
                 }
 
                 /*
@@ -125,22 +189,22 @@ public class BayesTraining {
                 System.out.println("n01: " + n01);
                 System.out.println("n10: " + n10);
                 System.out.println("n11: " + n11);
-                */
+                 */
                 
                 double n = n00 + n01 + n10 + n11; // size of training set
-                
+
                 // add 1 for Laplace smoothing
                 double f00 = (n * n00 + 1) / ((n00 + n01) * (n00 + n10) + 1);
                 double f01 = (n * n01 + 1) / ((n00 + n01) * (n01 + n11) + 1);
                 double f10 = (n * n10 + 1) / ((n10 + n11) * (n00 + n10) + 1);
                 double f11 = (n * n11 + 1) / ((n10 + n11) * (n01 + n11) + 1);
-                
+
                 /*
                 System.out.println("f00: " + f00);
                 System.out.println("f01: " + f01);
                 System.out.println("f10: " + f10);
                 System.out.println("f11: " + f11);
-                */
+                 */
                 
                 // use change of base to get log base-2
                 double score = (n00 / n) * (Math.log(f00) / Math.log(2))
@@ -151,7 +215,8 @@ public class BayesTraining {
                 /*
                 MutualInfo mi = new MutualInfo(term, author, score);
                 System.out.println(mi.toString());
-                */
+                 */
+                
                 miQueue.add(new MutualInfo(term, author, score));
 
             } // terms loop
@@ -192,11 +257,20 @@ public class BayesTraining {
 
                 public FileVisitResult visitFile(Path file,
                         BasicFileAttributes attrs) throws FileNotFoundException {
-                    // only process .txt files
-                    if (file.toString().endsWith(".txt")) {
+
+                    // index the disputed documents
+                    if (file.getParent().endsWith("DISPUTED")) {
+                        if (file.toString().endsWith(".txt")) {
+                            disputedFileNames.add(file.getFileName().toString());
+                            indexDisputedFile(file.toFile(), mDocumentID, index);
+                            mDocumentID++;
+                        }
+                    } else if (file.toString().endsWith(".txt")) {
+                        // process the training documents
                         indexFile(file.toFile(), mDocumentID, index);
                         mDocumentID++;
                     }
+
                     return FileVisitResult.CONTINUE;
                 }
 
@@ -215,8 +289,8 @@ public class BayesTraining {
     }
 
     /**
-     * Indexing, instantiate a TrainingDocument object the document, and
-     * collect the terms for the training set.
+     * Index, instantiate a TrainingDocument object for the file, and collect
+     * the terms for the training set.
      *
      * @param file
      * @param docID
@@ -235,7 +309,7 @@ public class BayesTraining {
             index.addTerm(term, docID);
             docTerms.add(term);
         }
-        
+
         // Increment the document count for the class
         switch (Authors.valueOf(file.getParentFile().getName())) {
             case HAMILTON:
@@ -254,52 +328,19 @@ public class BayesTraining {
     }
 
     /**
-     * @return the hTermProb
+     * Index the disputed file
+     *
+     * @param file
+     * @param docID
+     * @param index
+     * @throws FileNotFoundException
      */
-    public List<Double> getHamiltonProb() {
-        return hamiltonTermProb;
-    }
+    private void indexDisputedFile(File file, int docID, NaiveInvertedIndex index) throws FileNotFoundException {
+        ClassTokenStream s = new ClassTokenStream(file);
+        while (s.hasNextToken()) {
+            index.addTerm(s.nextToken(), docID);
+        }
 
-    /**
-     * @return the mTermProb
-     */
-    public List<Double> getMadisonProb() {
-        return madisonTermProb;
-    }
-
-    /**
-     * @return the jTermProb
-     */
-    public List<Double> getJayProb() {
-        return jayTermProb;
-    }
-
-    /**
-     * @return the hDocCount
-     */
-    public int getHamiltonCount() {
-        return hamiltonCount;
-    }
-
-    /**
-     * @return the mDocCount
-     */
-    public int getMadisonCount() {
-        return madisonCount;
-    }
-
-    /**
-     * @return the jDocCount
-     */
-    public int getJayCount() {
-        return jayCount;
-    }
-
-    /**
-     * @return the discSet
-     */
-    public List<String> getDiscSet() {
-        return discSet;
     }
 
 }
